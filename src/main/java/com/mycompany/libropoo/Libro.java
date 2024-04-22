145b3b3d5e8681ffee7e1c9bff7ecbbbc5792360
natach <danielfelipechg@gmail.com>
2024-04-22 10:35:30 -0500

    se agrega taller en clase

--- src/main/java/com/mycompany/libropoo/Libro.java
+++ src/main/java/com/mycompany/libropoo/Libro.java
@@ -1,5 +1,32 @@
 package com.mycompany.libropoo;
 
 public class Libro {
+private String titulo;
+private String autor;
+private String genero;
+private int anoPublicacion;
+boolean leido;
+
+public Libro(String titulo,String autor,String genero,int anoPublicacion ){
+this.titulo = titulo;
+this.autor = autor;
+this.genero = genero;
+this.anoPublicacion = anoPublicacion;
+this.leido= false;
+}
+
+public String mostrarInformacion(){
+    return "Título: " +titulo+", Autor: "+autor+", Año publicación: "+anoPublicacion+", Género: "+genero;
     
+}
+public void marcarLeido(){
+this.leido=true;
+}
+public boolean esAntiguo(){
+int anoActual = java.time.Year.now().getValue();
+return (anoActual-anoPublicacion)>50;
+}
+public boolean isLeido(){
+return leido;
+}
 }
--- src/main/java/com/mycompany/libropoo/Principal.java
+++ src/main/java/com/mycompany/libropoo/Principal.java
@@ -1,5 +1,32 @@
 package com.mycompany.libropoo;
 
+import java.util.Scanner;
+
 public class Principal {
+public static void main(String[] args){
+Scanner sc= new Scanner (System.in);
+
+    System.out.println("Ingrese el título: ");
+    String titulo = sc.nextLine();
+    System.out.println("Ingrese el autor del libro: ");
+    String autor = sc.nextLine();
+    System.out.println("Ingrese el año de publicación: ");
+    int anoPublicacion = sc.nextInt();
+    System.out.println("Ingrese el género del libro: ");
+    String genero= sc.nextLine();
+
+Libro libro1 = new Libro(titulo,autor,genero,anoPublicacion);
+    System.out.println(libro1.mostrarInformacion());
+    libro1.marcarLeido();
+    System.out.println(libro1.esAntiguo());
+    boolean esLeido = libro1.isLeido();
+    if(esLeido == true ){
+        System.out.println("El libro ya fue leido");
+        }
+    else{
+        System.out.println("El libro no ha sido leido");
+    }
     
+    sc.close();
+}
 }
